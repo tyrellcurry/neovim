052c23c971e4bff73486a9527c6d16f0949c6d21
tyrellcurry <tyrellcurryio@gmail.com>
2025-07-08 22:36:39 -0700

    Keymaps and lsp

--- lua/kickstart/plugins/nvim-lsp.lua
+++ lua/kickstart/plugins/nvim-lsp.lua
@@ -235,8 +235,8 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua',
-      'eslint-lsp',
-      'typescript-language-server',
+      -- 'eslint-lsp',
+      -- 'typescript-language-server',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
